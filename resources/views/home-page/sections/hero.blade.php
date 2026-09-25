3c9968aaccf2aab82f73171509506e1f618de1b1
ALMADIN_ITE2_ACT8 <ivanalmadin0@gmail.com>
2026-09-25 10:30:34 +0800

    Update hero.blade.php

--- resources/views/home-page/sections/hero.blade.php
+++ resources/views/home-page/sections/hero.blade.php
@@ -1,5 +1,5 @@
 <section class="hero-section" id="services">
-    <video class="hero-video" autoplay muted loop playsinline poster="{{ asset('home-bg.mp4') }}">
+    <video class="hero-video" autoplay muted loop playsinline poster="{{ asset('home-bg.png') }}">
         <source src="{{ asset('home-bg.mp4') }}" type="video/mp4">
     </video>
     <div class="hero-overlay"></div>
